b3193f977c91e787a8e3c41dfb7440a45fe1c1aa
nitheesh-max <86147100+nitheesh-max@users.noreply.github.com>
2021-10-02 23:01:03 +0530

    Create App.js

---
+++ grocerylist/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+//import Renderlist from './components/renderlist';
+import Grocery from './components/Grocery';
+//import Contact from './components/Contact';
+ function App() {
+  
+  return (
+    <div className="App">
+{/* <Renderlist/>  */}
+   <Grocery/> 
+  {/* <Contact/>   */}
+    </div>
+  );
+}
+
+export default App;
